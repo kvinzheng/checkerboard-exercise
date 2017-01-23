cc986c5d63683dde025c442fbe658acc64dd9d2c
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-01-22 23:05:05 -0800

    I put both the random color and black&red checkerboard on the same file

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 // Your JS goes here
+var body = document.body;
+
+
+
+//this is a red and black checkboard
+for(var i = 0; i < 81; i++){
+  var create = document.createElement('div');
+
+  if( i % 2 !== 0){
+    create.style.backgroundColor = 'rgb(255, 0, 0)';
+    create.style.width = '11.1%' ;
+    create.style.float = 'left';
+    create.style.paddingBottom = '11.1%';
+  }
+  else{
+    create.style.backgroundColor = "black";
+    create.style.width = '11.1%' ;
+    create.style.float = 'left';
+    create.style.paddingBottom = '11.1%';
+  }
+
+  body.append(create)
+}
+//return body;
+
+
+//this is a random color checkboard
+function getRandomColor(){
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for(var i = 0; i < 6; i++){
+    color = color + letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for(var i = 0; i < 81; i++){
+
+  var create = document.createElement('div');
+
+  if( i % 2 !== 0){
+    create.style.backgroundColor = getRandomColor();
+    create.style.width = '11.1%' ;
+    create.style.float = 'left';
+    create.style.paddingBottom = '11.1%';
+  }
+  else{
+    create.style.backgroundColor = getRandomColor();
+    create.style.width = '11.1%' ;
+    create.style.float = 'left';
+    create.style.paddingBottom = '11.1%';
+  }
+
+  body.append(create)
+}
